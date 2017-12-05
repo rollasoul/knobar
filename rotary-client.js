d257b3d8f932b1340fb460c7842166528b5561e5
pi <pi@rex-raspi3.pi>
2017-12-05 02:21:22 -0500

    Use fixed port on pi; Fix streaming bug

--- rotary-client.js
+++ rotary-client.js
@@ -1,26 +1,25 @@
 var SerialPort = require('serialport');
+const Readline = SerialPort.parsers.Readline;
 const WebSocket = require('ws');
 const ws = new WebSocket('wss://www.joyrats.com');
+//const ws = new WebSocket('ws://localhost:3000');
 
-var port = new SerialPort('/dev/cu.wchusbserial14610', {
+var port = new SerialPort('/dev/ttyUSB0', {
     baudRate: 115200
 });
 
-//const ws = new WebSocket('ws://localhost:3000');
+const parser = port.pipe(new Readline({ delimiter: '\r\n' }));
 
-
-// Switches the port into "flowing mode"
-// 
-port.on('data', function (data) {
+parser.on('data', function (data) {
   var input = parseInt(data.toString('ascii'));
-  console.log('Data:', input);
+  console.log('Data:', data.toString('ascii'));
   ws.send(input);
 });
 
 // Read data that is available but keep the stream from entering "flowing mode"
-port.on('readable', function () {
-  var data = port.read();
-  console.log('Readable:', data);
-  ws.send(data);
-});
-
+//port.on('readable', function () {
+//  var data = port.read();
+//  console.log('Readable:', data);
+//  ws.send(data);
+//});
+//
