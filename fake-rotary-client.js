dfd357e9e7adcac4002f2098dfe222f6f3c242be
Anthony Bui <anthonybui@gmail.com>
2017-12-11 03:18:51 -0500

    faker to test full pwm range

---
+++ fake-rotary-client.js
@@ -0,0 +1,15 @@
+const WebSocket = require('ws');
+const ws = new WebSocket('ws://www.joyrats.com');
+
+//const ws = new WebSocket('ws://localhost:3000');
+
+
+var input = 0;
+
+function publish() {
+  input = (input + 1) % 40;
+  console.log('Data:', input);
+  ws.send(input);
+}
+
+setInterval(publish, 100);
